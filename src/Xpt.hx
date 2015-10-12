61c371a665c33cffa16d8a4b54d17bfc13754c4c
Ian Harrigan <ianharrigan@hotmail.com>
2015-10-12 21:39:10 +0200

    Revert relevant changes in branch

--- src/Xpt.hx
+++ src/Xpt.hx
@@ -1,15 +1,13 @@
 package;
 
 import code.Code;
-import devel.Devel;
-import openfl.display.Sprite;
 import openfl.Lib;
 import xpt.error.ErrorMessage;
 import xpt.start.WebStart;
 import xpt.trial.Trial;
 
 
-class Xpt  extends Sprite  {
+class Xpt {
 
 	private static var localExptDirectory:String = "./experiments/";
 	
@@ -23,24 +21,11 @@ class Xpt  extends Sprite  {
 		#if debug
 			Trial.testing = true;
 			Code.testing = true;
-			//var tests:xpt.Tests = new xpt.Tests();
+			var tests:xpt.Tests = new xpt.Tests();
 			Trial.testing = false;
 			Code.testing = false;
 		#end
 		
 		var webStart:WebStart = new WebStart(dir, expt);
-		
-		
-		
-		
-	}
-	
-	public function new() {
-		super();
-		trace(11);
-		var rect:Sprite = new Sprite();
-		rect.graphics.beginFill(0x333333);
-		rect.graphics.drawCircle(200, 200, 50);
-		this.addChild(rect);
 	}
 }
