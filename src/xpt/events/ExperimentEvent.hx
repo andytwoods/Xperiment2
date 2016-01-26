e0424defd0b2baa91b5d19b176543e1109bb64ad
Ian Harrigan <ianharrigan@hotmail.com>
2016-01-26 12:15:22 +0100

    Added experiment event class

---
+++ src/xpt/events/ExperimentEvent.hx
@@ -0,0 +1,16 @@
+package xpt.events;
+
+import openfl.events.Event;
+import xpt.trial.Trial;
+
+class ExperimentEvent extends Event {
+    public static inline var TRAIL_START:String = "TrailStart";
+    public static inline var TRAIL_END:String = "TrailEnd";
+    
+    public var trail(default, default):Trial;
+    
+    public function new(type:String) {
+        super(type);
+    }
+    
+}
