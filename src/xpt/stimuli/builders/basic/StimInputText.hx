ca1e99c81cba3ca3750e485473ea0832c07879d4
Ian Harrigan <ianharrigan@hotmail.com>
2016-01-31 13:55:40 +0100

    html5 work arounds

--- src/xpt/stimuli/builders/basic/StimInputText.hx
+++ src/xpt/stimuli/builders/basic/StimInputText.hx
@@ -1,8 +1,14 @@
 package xpt.stimuli.builders.basic;
 
+import code.Scripting;
 import haxe.ui.toolkit.controls.TextInput;
 import haxe.ui.toolkit.core.Component;
 import haxe.ui.toolkit.events.UIEvent;
+import haxe.ui.toolkit.text.ITextDisplay;
+import openfl.events.Event;
+import openfl.events.KeyboardEvent;
+import openfl.text.TextField;
+import xpt.events.ExperimentEvent;
 import xpt.stimuli.StimulusBuilder;
 
 class StimInputText extends StimulusBuilder {
@@ -12,10 +18,30 @@ class StimInputText extends StimulusBuilder {
     
     private override function createComponentInstance():Component {
         var input:TextInput = new TextInput();
-        input.addEventListener(UIEvent.CHANGE, function(e:UIEvent) {
-			trace(111, input.text, 22);
+        
+        #if html5
+        // supid openfl workarounds for html5 - onchange never fired
+        input.sprite.addEventListener(KeyboardEvent.KEY_DOWN, function(e) {
            onStimValueChanged(input.text); 
         });
+        input.sprite.addEventListener(KeyboardEvent.KEY_UP, function(e) {
+           onStimValueChanged(input.text); 
+        });
+        
+        // this stupid one is so the caret appears when the text is 0 length
+        var textDisplay:ITextDisplay = Reflect.field(input, "_textDisplay");
+        var textField:TextField = cast textDisplay.display;
+        textField.border = true;
+        textField.borderColor = 0xFFFFFF;
+        
+        #else
+        
+        input.addEventListener(UIEvent.CHANGE, function(e:UIEvent) {
+           onStimValueChanged(input.text); 
+        });
+        
+        #end
+
         return input;
     }
     
