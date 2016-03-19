c42d9121571d63884a2f13dee9cfab86900dc8b9
andytwoods <andy.woods@xperiment.mobi>
2016-03-19 09:28:22 +0000

    fakeCaret

--- src/xpt/stimuli/builders/basic/StimInputText.hx
+++ src/xpt/stimuli/builders/basic/StimInputText.hx
@@ -5,46 +5,75 @@ import haxe.ui.toolkit.controls.TextInput;
 import haxe.ui.toolkit.core.Component;
 import haxe.ui.toolkit.events.UIEvent;
 import haxe.ui.toolkit.text.ITextDisplay;
+import openfl.display.Sprite;
 import openfl.events.Event;
+import openfl.events.FocusEvent;
 import openfl.events.KeyboardEvent;
+import openfl.events.MouseEvent;
+import openfl.events.TimerEvent;
 import openfl.text.TextField;
+import openfl.utils.Timer;
 import xpt.events.ExperimentEvent;
+import xpt.stimuli.builders.basic.StimInputText.FakeCaret;
 import xpt.stimuli.StimulusBuilder;
 
 class StimInputText extends StimulusBuilder {
+	#if html5
+		var fakeCaret:FakeCaret;
+	#end
+	
     public function new() {
         super();
+		
+    }
+	
+	override public function onRemovedFromTrial() {
+		#if html5
+			fakeCaret.kill();
+			fakeCaret = null;
+		#end
+		super.onRemovedFromTrial();
     }
     
     private override function createComponentInstance():Component {
         var input:TextInput = new TextInput();
         
         #if html5
-        // supid openfl workarounds for html5 - onchange never fired
-        input.sprite.addEventListener(KeyboardEvent.KEY_DOWN, function(e) {
-           onStimValueChanged(input.text); 
-        });
-        input.sprite.addEventListener(KeyboardEvent.KEY_UP, function(e) {
-           onStimValueChanged(input.text); 
-        });
-        
-        // this stupid one is so the caret appears when the text is 0 length
-        var textDisplay:ITextDisplay = Reflect.field(input, "_textDisplay");
-        var textField:TextField = cast textDisplay.display;
-        textField.border = true;
-        textField.borderColor = 0xFFFFFF;
-        
+			if (this.fakeCaret != null) fakeCaret.kill();
+			this.fakeCaret = new FakeCaret(input);
+			this.fakeCaret.x = 10;
+			
+		
+			// stupid openfl workarounds for html5 - onchange never fired
+			input.sprite.addEventListener(KeyboardEvent.KEY_DOWN, function(e) {
+			   fakeCaret.onLength(input.text.length);
+			   onStimValueChanged(input.text); 
+			});
+			input.sprite.addEventListener(KeyboardEvent.KEY_UP, function(e) {
+			   onStimValueChanged(input.text); 
+			});
+			
+			input.sprite.addEventListener(MouseEvent.CLICK, function(e) { 
+				input.style.borderSize = 2;
+				if (input.text.length == 0) fakeCaret.on();
+		
+			} );
+			input.sprite.addEventListener(FocusEvent.FOCUS_OUT, function(e) { 
+				input.style.borderSize = 1;
+			} );
+	
         #else
         
-        input.addEventListener(UIEvent.CHANGE, function(e:UIEvent) {
-           onStimValueChanged(input.text); 
-        });
-        
+			input.addEventListener(UIEvent.CHANGE, function(e:UIEvent) {
+			   onStimValueChanged(input.text); 
+			});
+			
         #end
 
         return input;
     }
-    
+	
+
     private override function applyProperties(c:Component) {
         super.applyProperties(c);
     }
@@ -53,5 +82,49 @@ class StimInputText extends StimulusBuilder {
 		var map:Map<String,String> = new Map<String,String>();
 		map.set('', _stim.value);
 		return map;
+	}
+}
+
+
+class FakeCaret extends Sprite {
+	var t:Timer;
+	var p:Component;
+	
+	public function new(parent:Component) {
+		super();
+		this.graphics.lineStyle(.5);
+		this.graphics.lineTo(0, 20);
+		this.y = 15;
+		
+		this.t = new Timer(500, 100);
+		t.addEventListener(TimerEvent.TIMER, blinkL);
+		this.p = parent;
+		
+	}
+	
+	public function on() {
+		t.start();
+		p.sprite.addChild(this);
+	}
+	
+	public function onLength(i:Int) {
+		if (i == 0) on();
+		else off();
+	}
+	
+	public function off() {
+		t.stop();
+		p.sprite.removeChild(this);	
+	}
+	
+	private function blinkL(e:TimerEvent):Void 
+	{
+		this.visible = !this.visible;
+	}
+	
+	public function kill() {
+		t.stop();
+		t.removeEventListener(TimerEvent.TIMER, blinkL);
+		parent = null;
 	}
 }
