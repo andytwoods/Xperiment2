32451d84bec5330d660232ea2789e78789bc4759
andytwoods <andy.woods@xperiment.mobi>
2016-03-20 09:07:24 +0000

    fixed handcursor bug with links in html5

--- src/xpt/stimuli/builders/basic/StimLink.hx
+++ src/xpt/stimuli/builders/basic/StimLink.hx
@@ -1,6 +1,7 @@
 package xpt.stimuli.builders.basic;
 import haxe.ui.toolkit.controls.Link;
 import haxe.ui.toolkit.core.Component;
+import openfl.display.Sprite;
 import xpt.stimuli.StimulusBuilder;
 
 class StimLink extends StimulusBuilder {
@@ -15,7 +16,21 @@ class StimLink extends StimulusBuilder {
 	private override function applyProperties(c:Component) {
 		super.applyProperties(c);
         
+		
+		
         var link:Link = cast c;
+		
+		#if html5 
+			var s:Sprite = new Sprite();
+			s.useHandCursor = true;
+			s.buttonMode =  true;
+			
+			s.graphics.beginFill(0xffffff, 0);
+			s.graphics.drawRect(0, 0, link.width, link.height);
+			link.sprite.addChildAt(s, 0);		
+		#end
+		
+
         if (get("url") != null) {
             link.url = get("url");
         }
