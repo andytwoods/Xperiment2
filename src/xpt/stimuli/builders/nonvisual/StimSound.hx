b5a66112f34fec68392c008ab56301113fa51c8f
andytwoods <andytwoods@gmail.com>
2016-07-03 15:28:31 +0100

    part way to adding buttons to sound

--- src/xpt/stimuli/builders/nonvisual/StimSound.hx
+++ src/xpt/stimuli/builders/nonvisual/StimSound.hx
@@ -1,6 +1,8 @@
 package xpt.stimuli.builders.nonvisual;
 
+import haxe.ui.toolkit.controls.Button;
 import haxe.ui.toolkit.core.Component;
+import openfl.events.MouseEvent;
 import openfl.media.SoundChannel;
 import xpt.experiment.Preloader;
 import xpt.debug.DebugManager;
@@ -13,15 +15,17 @@ class StimSound extends StimulusBuilder {
 	
 	private var my_sound:Sound;
 	var soundChannel:SoundChannel;
-
+	var play:Button;
+	var pause:Button;
+	var selectButton:Button;
+		
     public function new() {
         super();
     }
     
 	private override function applyProperties(c:Component) {
-        c.visible = false;    
+        
 		
-
 		var resource:String = get("resource");
 
 		if (resource != null) {
@@ -36,6 +40,73 @@ class StimSound extends StimulusBuilder {
 				});
 			}
 		}
+		
+		var controls:String = get("controls", "none");
+		if (controls != "none") {
+			
+			var select:String = get("select", '' );
+			
+			super.applyProperties(c);
+			setup_buttons(c, select);
+
+		}
+		else{
+			c.visible = false;    
+		}
+
+	}
+	
+	inline function setup_buttons(c:Component, select:String) 
+	{
+		if (pause == null) {
+			pause = new Button();
+			play = new Button();
+			pause.addEventListener(MouseEvent.CLICK, mouseL);
+			play.addEventListener(MouseEvent.CLICK, mouseL);
+		};
+	
+		
+		pause.icon = "img/icons/pause.png";
+		play.icon = "img/icons/play.png";
+
+		
+		if(select != ''){
+			
+			if (selectButton == null) {
+				selectButton = new Button();
+		
+				selectButton.addEventListener(MouseEvent.CLICK, selectL);
+			}						
+		}
+
+		play.iconPosition = pause.iconPosition = 'center';
+		
+		c.addChild(pause);
+		c.addChild(play);
+		pause.x = c.width * .51;
+		play.percentWidth = pause.percentWidth = 49;
+		
+		if (selectButton != null) {
+			c.addChild(selectButton);
+			play.percentHeight = pause.percentHeight = selectButton.percentHeight = 49;
+			selectButton.y = c.height * .51;
+			selectButton.text = select;	
+		}
+	}
+	
+	private function mouseL(e:MouseEvent):Void 
+	{
+		if (e.currentTarget == play) {
+			if(my_sound!=null) my_sound.play();
+		}
+		else {
+			if(soundChannel!=null) this.soundChannel.stop();
+		}
+	}
+	
+	private function selectL(e:MouseEvent):Void 
+	{
+		runScriptEvent("action", e);
 	}
 	
 	function setSound(sound:Sound) 
